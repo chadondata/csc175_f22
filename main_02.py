cead2c887bd9572af03d7365060ba14ebfa2cf49
Chad Harper <chad.harper@gmail.com>
2022-10-31 16:53:34 -0400

    Class work 10/31

--- main_02.py
+++ main_02.py
@@ -10,15 +10,32 @@ import turtle
 
 WELCOME = "Python Drawing Application"
 
+def draw(t, direction, distance):
+    if direction == 'left':
+        t.left(distance)
+    elif direction == 'forward':
+        t.forward(distance)
+
 def initialize_turtle():
     return turtle.Turtle()
+
+def process_draw_steps(t, draw_steps):
+    for i in range(len(draw_steps)):
+        draw(t, draw_steps[i][0], draw_steps[i][1])
 
 def main():
     print(WELCOME)
     main_turtle = initialize_turtle()
-    main_turtle.forward(100)
-    main_turtle.left(90)
-    main_turtle.forward(100)
+
+    draw_steps = [
+        ['forward', 100]
+        , ['left', 90]
+        , ['forward', 100]
+        , ['left', 45]
+        , ['forward', 75]
+    ]
+    process_draw_steps(main_turtle, draw_steps)
+    
     turtle.Screen().exitonclick()
 
 if __name__ == '__main__':
--- string_list.py
+++ string_list.py
@@ -1,3 +1,7 @@
 x = 'Today was good. Today was fun. Tomorrow is another one.'
 for i in range(len(x)):
     print('Character at position %i: %s' % (i, x[i]))
+
+for l in x:
+    print(l)
+    
