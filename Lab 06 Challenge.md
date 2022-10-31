b4dbf4e632fc9995faa0e8c2cbb3a000df82ed3f
Chad Harper <chad.harper@gmail.com>
2022-10-31 15:30:23 -0400

    Class work 10/31

---
+++ Lab 06 Challenge.md
@@ -0,0 +1,17 @@
+# Lab 06 Challenge
+For this week's challenge, add a function to your code called draw that takes the following parameters:
+
+    * a turtle
+    * a string
+    * a number
+The turtle will be the same turtle you initialized in the main function passed to this function. It will do the drawing.
+
+Possible values for the string will be "left" or "forward"
+
+the number will be a positive integer
+
+## In the function:
+
+If the value of the string is "left", tell the turtle to turn left using the number in the third parameter
+If the value of the string is "forward", tell the turtle to move forward using the number in the third parameter
+Lastly, add some function calls to your main function to test your new draw function.
--- main.py
+++ main.py
@@ -13,15 +13,40 @@ import turtle
 # Globals and constants
 APPLICATION_NAME = "Python Drawing App v1"
 
+def draw(t, direction, distance):
+    if direction == 'left':
+        t.left(distance)
+    elif direction == 'forward':
+        t.forward(distance)
+
 def initialize_turtle():
     return turtle.Turtle()
 
+def process_draw_list(t, draw_list):
+    for i in range(len(draw_list)):
+        draw(t, draw_list[i][0], draw_list[i][1])
+
 def main():
     print(APPLICATION_NAME)
+    directions = ['forward', 'left', 'forward', 'left', 'forward']
+    distances = [100, 90, 100, 90, 100]
     main_turtle = initialize_turtle()
-    main_turtle.forward(100)
-    main_turtle.left(90)
-    main_turtle.forward(100)
+
+    draw_list = [
+        ['forward', 100]
+        , ['left', 90]
+        , ['forward', 100]
+        , ['left', 90]
+        , ['forward', 90]
+        , ['left', 45]
+        , ['forward', 100]
+    ]
+
+    process_draw_list(main_turtle, draw_list)
+
+    # for i in range(len(directions)):
+    #    draw(main_turtle, directions[i], distances[i])
+
     turtle.Screen().exitonclick()
 
 if __name__ == '__main__':
