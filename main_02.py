8f8fd81991db6f4f9f3478e1841627cd9469cd4a
Chad Harper <chad.harper@gmail.com>
2022-11-02 16:54:16 -0400

    Class work 11/2

--- main_02.py
+++ main_02.py
@@ -10,6 +10,29 @@ import turtle
 
 WELCOME = "Python Drawing Application"
 
+    # action_list = [
+    #     ('forward', 100)
+    #     , ('left', 90)
+    #     , ('forward', 100)
+    #     , ('left', 45)
+    #     , ('forward', 75)
+    # ]
+
+def get_actions():
+    action_list = []
+    action_list.append(('forward', 100))
+    action_list.append(('left', 90))
+    action_list.append(('forward', 100))
+    action_list.append(('left', 45))
+    
+    # The following code is a good start for this week's programming challenge
+    action = 'forward'
+    distance = 75
+    vector = (action, distance)
+    action_list.append(vector)
+
+    return action_list
+
 def draw(t, direction, distance):
     if direction == 'left':
         t.left(distance)
@@ -27,14 +50,10 @@ def main():
     print(WELCOME)
     main_turtle = initialize_turtle()
 
-    draw_steps = [
-        ['forward', 100]
-        , ['left', 90]
-        , ['forward', 100]
-        , ['left', 45]
-        , ['forward', 75]
-    ]
-    process_draw_steps(main_turtle, draw_steps)
+    # Tuple - Multiple values stored in one variable
+    draw_steps_tuples = get_actions()
+
+    process_draw_steps(main_turtle, draw_steps_tuples)
     
     turtle.Screen().exitonclick()
 
