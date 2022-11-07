04bac5367ccf2f88357a0280c110cef9000e2173
Chad Harper <chad.harper@gmail.com>
2022-11-07 16:57:45 -0500

    Class work 11/7

--- main_02.py
+++ main_02.py
@@ -18,6 +18,37 @@ WELCOME = "Python Drawing Application"
     #     , ('forward', 75)
     # ]
 
+def get_actions_prompt():
+    action_list = []
+    prompt = """
+Enter an action from the list below:
+    [0] forward
+    [1] left
+    [2] right
+    [q] quit
+Your choice:  """
+
+    keep_going = True
+    while keep_going:
+        response = input(prompt)
+        if response == 'q':
+            keep_going = False
+        else:
+            response = int(response)
+            if response == 0:
+                action = 'forward'
+            elif response == 1:
+                action = 'left'
+            elif response == 2:
+                action = 'right'
+            else:
+                action = 'NA'
+            distance = eval(input('Enter a value for the distance: '))
+            vector = (action, distance)
+            action_list.append(vector)
+    
+    return action_list
+
 def get_actions():
     action_list = []
     action_list.append(('forward', 100))
@@ -36,8 +67,12 @@ def get_actions():
 def draw(t, direction, distance):
     if direction == 'left':
         t.left(distance)
+    elif direction == 'right':
+        t.right(distance)
     elif direction == 'forward':
         t.forward(distance)
+    else:
+        print(f'{direction} not implemented.')
 
 def initialize_turtle():
     return turtle.Turtle()
@@ -51,7 +86,8 @@ def main():
     main_turtle = initialize_turtle()
 
     # Tuple - Multiple values stored in one variable
-    draw_steps_tuples = get_actions()
+    # draw_steps_tuples = get_actions()
+    draw_steps_tuples = get_actions_prompt()
 
     process_draw_steps(main_turtle, draw_steps_tuples)
     
