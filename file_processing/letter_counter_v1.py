4b6dade0b2391bc53b848d42e1528ba8aa55d651
Chad Harper <chad.harper@gmail.com>
2022-12-07 15:14:55 -0500

    Add files via upload

--- file_processing/letter_counter_v1.py
+++ file_processing/letter_counter_v1.py
@@ -4,39 +4,47 @@ import string
 file_contents = []
 letters = {}
 
-with open('alice.txt', encoding='utf8') as the_file:
-    this_line = the_file.readline() # Priming read
-    while this_line:
-        file_contents.append(this_line)
-        this_line = the_file.readline()
+def get_filename():
+    file_name = input('Enter a file name: ')
+    return file_name
 
-for line in file_contents:
-    for char in line:
-        if char.isalpha():
-            char = char.lower()
-            if char in letters.keys():
-                letters[char] += 1
-            else:
-                letters[char] = 1
+def main():
+    with open(get_filename(), encoding='utf8') as the_file:
+        this_line = the_file.readline() # Priming read
+        while this_line:
+            file_contents.append(this_line)
+            this_line = the_file.readline()
 
-for letter in letters.keys():
-    print(f'{letter} : {letters[letter]}')
+    for line in file_contents:
+        for char in line:
+            if char.isalpha():
+                char = char.lower()
+                if char in letters.keys():
+                    letters[char] += 1
+                else:
+                    letters[char] = 1
 
-print("Alphabetic:")
-for letter in sorted(letters.keys()):
-    print(f'{letter} : {letters[letter]}')
+    for letter in letters.keys():
+        print(f'{letter} : {letters[letter]}')
 
-print("Percentages:")
-total_letters = 0
-for letter in letters.keys():
-    total_letters += letters[letter]
-print(f'Total letters: {total_letters}')
+    print("Alphabetic:")
+    for letter in sorted(letters.keys()):
+        print(f'{letter} : {letters[letter]}')
 
-distributions = {}
-print("Percentages:")
-for letter in sorted(letters.keys()):
-    print(f'{letter} : {round((letters[letter] * 100) / total_letters)}')
-    distributions[letter] = round((letters[letter] * 100) / total_letters)
+    print("Percentages:")
+    total_letters = 0
+    for letter in letters.keys():
+        total_letters += letters[letter]
+    print(f'Total letters: {total_letters}')
 
-for letter in distributions.keys():
-    print(f'{letter} : {distributions[letter]} : {"*" * distributions[letter]}')
+    distributions = {}
+    print("Percentages:")
+    for letter in sorted(letters.keys()):
+        print(f'{letter} : {round((letters[letter] * 100) / total_letters)}')
+        distributions[letter] = round((letters[letter] * 100) / total_letters)
+
+    for letter in distributions.keys():
+        print(f'{letter} : {distributions[letter]} : {"*" * distributions[letter]}')
+
+if __name__ == '__main__':
+    main()
